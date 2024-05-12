5db0709ca480f130c22207633faa1b79b0f66335
Christian Miranda <chrisjav28@hotmail.com>
2024-05-12 08:53:39 -0300

    modif

---
+++ instituto.php
@@ -0,0 +1,24 @@
+
+{
+    "nombre": "Instituto XYZ",
+    "direccion": "Calle Principal #123",
+    "ciudad": "Ciudad ABC",
+    "telefono": "(123) 456-7890",
+    "coordenadas":[
+        {"x":-27.449005920328197,
+        "y":-58.98585214712169}
+        ],
+    "ofertas": [
+        {
+            "nombre": "Profesorado en Tecnología",
+            "duracion": 4,
+            "descripcion": "Para los años ..."
+        },
+        {
+            "nombre": "Tecnicatura en Seguridad e Higiene",
+            "duracion": 3,
+            "descripcion": "El egresado de la tecnicatura.....",
+            "campo_laboral": "Empresas, Instituciones gubernamentales,..."
+        }
+    ]
+}
